054e53df37b634476fdf1f77d5cc89e4589b162b
pronsin <33540506+pronsin@users.noreply.github.com>
2020-03-07 22:20:43 +0700

    Add files via upload

---
+++ UIQuiz/app/src/main/java/com/example/uiquiz/a.kt
@@ -0,0 +1,12 @@
+package com.example.uiquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class a : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_a)
+    }
+}
